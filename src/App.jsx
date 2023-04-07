f36ede36c84933f7e3d589e9fb1cf6a72a43d385
VillalbaAgustin <villalba.agus.99@gmail.com>
2023-04-07 13:02:47 -0300

    add class nav__hidden

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,34 @@ import { Navbar } from "./components/Navbar";
 
 export const App = () => {
    return (
-      <>
+      <div>
          <Navbar/>
-         {/* <div>
+         <div>
             <h2>
                <b>01.</b> Profile
             </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta laudantium id libero qui praesentium repellendus ipsa dolorem odio saepe, deleniti aliquid suscipit, fugiat architecto. Delectus similique cupiditate dignissimos exercitationem possimus.</p>
          </div>
          <div>
             <h2>
                <b>02.</b> Skills
             </h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem, dolor labore alias ab, vel quia vitae distinctio commodi in, perferendis assumenda laudantium voluptatibus aperiam iure est atque ratione placeat.</p>
          </div>
          <div>
             <h2>
                <b>03.</b> Projects
             </h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem, dolor labore alias ab, vel quia vitae distinctio commodi in, perferendis assumenda laudantium voluptatibus aperiam iure est atque ratione placeat.</p>
          </div>
          <div>
             <h2>
                <b>04.</b> Get in touch
             </h2>
-         </div> */}
-      </>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem, dolor labore alias ab, vel quia vitae distinctio commodi in, perferendis assumenda laudantium voluptatibus aperiam iure est atque ratione placeat.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem, dolor labore alias ab, vel quia vitae distinctio commodi in, perferendis assumenda laudantium voluptatibus aperiam iure est atque ratione placeat.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolorem, dolor labore alias ab, vel quia vitae distinctio commodi in, perferendis assumenda laudantium voluptatibus aperiam iure est atque ratione placeat.</p>
+         </div>
+      </div>
    );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { getTopNav } from "../data/navbar";
 
 export const Navbar = () => {
    const [navItems, setNavItems] = useState([]);
-   const [collapse, setCollapse] = useState("nav__menu");
+   const [collapse, setCollapse] = useState("nav__menu nav__hidden");
    const [toggleIcon, setToggleIcon] = useState("toggler__icon");
 
    useEffect(() => {
@@ -11,9 +11,9 @@ export const Navbar = () => {
    }, []);
 
    const onToggle = () => {
-      collapse === "nav__menu"
+      collapse === "nav__menu nav__hidden"
          ? setCollapse("nav__menu nav__collapse")
-         : setCollapse("nav__menu");
+         : setCollapse("nav__menu nav__hidden");
 
       toggleIcon === "toggler__icon"
          ? setToggleIcon("toggler__icon toggle")
@@ -38,13 +38,13 @@ export const Navbar = () => {
                      </li>
                   ))}
                </ul>
-            <div className="container__toggle">
-               <div className={toggleIcon} onClick={onToggle}>
-                  <div className="line__1"></div>
-                  <div className="line__2"></div>
-                  <div className="line__3"></div>
+               <div className="container__toggle">
+                  <div className={toggleIcon} onClick={onToggle}>
+                     <div className="line__1"></div>
+                     <div className="line__2"></div>
+                     <div className="line__3"></div>
+                  </div>
                </div>
-            </div>
             </nav>
          </div>
       </header>
