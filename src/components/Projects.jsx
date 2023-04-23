dcc171729075eab4229c8d7b3142f0f592edb85b
Agustín Villalba <villalba.agus.99@gmail.com>
2023-04-22 22:27:57 -0300

    function goToSlides added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,19 +27,21 @@ const slides = [
 export const Projects = () => {
    const [currentIndex, setCurrentIndex] = useState(3);
 
-   
-   const goToPrevious = () =>{
-      const isFirstSlide = currentIndex === 0 ;
-      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex -1;
+   const goToPrevious = () => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
       setCurrentIndex(newIndex);
-   }
+   };
 
-   const goToNext = () =>{
-      const isLastSlide = currentIndex === slides.length -1;
-      const newIndex = isLastSlide ? 0 : currentIndex +1;
-
+   const goToNext = () => {
+      const isLastSlide = currentIndex === slides.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
       setCurrentIndex(newIndex);
-   }
+   };
+
+   const goToSlide = (slideIndex) => {
+      setCurrentIndex(slideIndex);
+   };
 
    return (
       <div id="Projects" className="card__container ">
@@ -49,7 +51,10 @@ export const Projects = () => {
             </h2>
             <div className="slider__container">
                <div className="arrow">
-                  <i className="bi bi-caret-left-fill" onClick={goToPrevious}></i>
+                  <i
+                     className="bi bi-caret-left-fill"
+                     onClick={goToPrevious}
+                  ></i>
                </div>
                <div className="img">
                   <img
@@ -60,6 +65,19 @@ export const Projects = () => {
                <div className="arrow">
                   <i className="bi bi-caret-right-fill" onClick={goToNext}></i>
                </div>
+            </div>
+            <div className="dot__container">
+               {slides.map((slide, slideIndex) => (
+                  <div
+                     className={
+                        currentIndex === slideIndex ? "active__dot" : "dot"
+                     }
+                     onClick={() => goToSlide(slideIndex)}
+                     key={slideIndex}
+                  >
+                     <i className="bi bi-dot"></i>
+                  </div>
+               ))}
             </div>
          </div>
       </div>
