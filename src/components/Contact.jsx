bdbec61ec8fc484aa84338aebcd78e5383d06ee2
VillalbaAgustin <villalba.agus.99@gmail.com>
2023-04-26 19:03:56 -0300

    contact form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,76 +1,57 @@
 import React from "react";
+import "./Contact.css";
 
 export const Contact = () => {
    return (
       <>
-      <div id="Contact" className="href"/>
-      <div id="" className="card__container">
-         <div className="">
-            <h2>
-               <b>4.</b> Get in touch
-            </h2>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-               dolorem, dolor labore alias ab, vel quia vitae distinctio commodi
-               in, perferendis assumenda laudantium voluptatibus aperiam iure
-               est atque ratione placeat.
-            </p>
+         <div id="Contact" className="href" />
+         <div id="" className="card__container">
+            <div className="">
+               <h2>
+                  <b>4.</b> Get in touch
+               </h2>
+               <form
+                  target="_blank"
+                  action="https://formsubmit.co/villalba.4gustin@gmail.com"
+                  method="POST"
+               >
+                  <div className="form-group">
+                     <div className="form-row">
+                        <div className="col">
+                           <input
+                              type="text"
+                              name="name"
+                              className="input__form"
+                              placeholder="Full Name"
+                              required
+                           />
+                        </div>
+                        <div className="col">
+                           <input
+                              type="email"
+                              name="email"
+                              className="input__form"
+                              placeholder="Email Address"
+                              required
+                           />
+                        </div>
+                     </div>
+                  </div>
+                  <div className="">
+                     <textarea
+                        placeholder="Your Message"
+                        className="input__form"
+                        name="message"
+                        rows="10"
+                        required
+                     ></textarea>
+                  </div>
+                  <button type="submit" className="btn__form">
+                     Submit
+                  </button>
+               </form>
+            </div>
          </div>
-      </div>
       </>
    );
 };
