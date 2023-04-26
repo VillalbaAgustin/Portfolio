ba00eb28b08671c62bd789b9ed8c417c5f4c72f7
Agustín Villalba <villalba.agus.99@gmail.com>
2023-04-25 23:36:46 -0300

    icons added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,21 +6,29 @@ const slides = [
       url: "./projects/CalendarAppLogin.JPG",
       title: "CalendarApp",
       description: "Hola Mundo",
+      href: "https://calendar-backend-production-9efe.up.railway.app/",
+      hrefGit: "https://github.com/VillalbaAgustin/Calendar.git",
    },
    {
       url: "./projects/GifApp.JPG",
       title: "GifApp",
       description: "Hola Mundo",
+      href: "https://villalbaagustin.github.io/react-gifexpertapp/",
+      hrefGit: "https://github.com/VillalbaAgustin/react-gifexpertapp.git",
    },
    {
       url: "./projects/JournalApp.JPG",
       title: "JournalApp",
       description: "Hola Mundo",
+      href: "https://journal-app-neon-eight.vercel.app/auth/login",
+      hrefGit: "https://github.com/VillalbaAgustin/JournalApp.git",
    },
    {
       url: "./projects/MovieApp.JPG",
       title: "MovieApp",
       description: "Hola Mundo",
+      href: "https://villalbaagustin.github.io/movie-react/",
+      hrefGit: "https://github.com/VillalbaAgustin/movie-react.git",
    },
 ];
 
@@ -45,44 +53,50 @@ export const Projects = () => {
 
    return (
       <>
-      <div id="Projects" className="href"/>
-      <div id="" className="card__container ">
-         <div className="projects__container">
-            <h2>
-               <b>3.</b> Projects
-            </h2>
-            <div className="slider__container">
-               <div className="arrow">
-                  <i
-                     className="bi bi-caret-left-fill"
-                     onClick={goToPrevious}
-                  ></i>
-               </div>
-               <div className="img">
-                  <img
-                     src={slides[currentIndex].url}
-                     alt={slides[currentIndex].title}
-                  />
-               </div>
-               <div className="arrow">
-                  <i className="bi bi-caret-right-fill" onClick={goToNext}></i>
-               </div>
-            </div>
-            <div className="dot__container">
-               {slides.map((slide, slideIndex) => (
-                  <div
-                     className={
-                        currentIndex === slideIndex ? "active__dot" : "dot"
-                     }
-                     onClick={() => goToSlide(slideIndex)}
-                     key={slideIndex}
-                  >
-                     <i className="bi bi-dot"></i>
+         <div id="Projects" className="href" />
+         <div id="" className="card__container ">
+            <div className="projects__container">
+               <h2>
+                  <b>3.</b> Projects
+               </h2>
+               <div className="slider__container">
+                  <div className="arrow">
+                     <i
+                        className="bi bi-chevron-left"
+                        onClick={goToPrevious}
+                     ></i>
                   </div>
-               ))}
+                  <div className="img">
+                     <img
+                        src={slides[currentIndex].url}
+                        alt={slides[currentIndex].title}
+                     />
+                  </div>
+                  <div className="arrow">
+                     <i className="bi bi-chevron-right" onClick={goToNext}></i>
+                  </div>
+               </div>
+               <div className="dot__container">
+                  <a className="href__git" target="_blank" href={slides[currentIndex].hrefGit}>
+                     <i className="bi bi-github"></i>
+                  </a>
+                  {slides.map((slide, slideIndex) => (
+                     <div
+                        className={
+                           currentIndex === slideIndex ? "active__dot" : "dot"
+                        }
+                        onClick={() => goToSlide(slideIndex)}
+                        key={slideIndex}
+                     >
+                        <i className="bi bi-dot"></i>
+                     </div>
+                  ))}
+                  <a className="href__page" target="_blank" href={slides[currentIndex].href}>
+                     <i className="bi bi-link"></i>
+                  </a>
+               </div>
             </div>
          </div>
-      </div>
       </>
    );
 };
